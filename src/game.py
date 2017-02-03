9799c921d9a010a364037cf2826d41b2eda3633e
Brady Sheehan <Bradya.sheehan@gmail.com>
2017-02-03 17:06:18 -0500

    practice stuff

---
+++ src/game.py
@@ -0,0 +1,12 @@
+class Game():
+
+
+if __name__ == '__main__':
+	Game game = new Game()
+
+	if event_takes_place:
+		game.start_new_game()
+
+	#neeed to decide on a number of "lives" or balls
+	#need to figure out where the ball gets "generated", how the ball gets released, the typical trajectory it follows
+	#will there be any aspects of our game that are animate? any moving objects other than the balls?
