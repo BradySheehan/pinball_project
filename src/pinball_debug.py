393681303191203d496f298b3550d641badd78d7
Brady Sheehan <Bradya.sheehan@gmail.com>
2017-03-19 20:54:49 -0400

    Added the rendering of four walls and their coressponding OdeGeom objects such that the ball will collide with them

--- src/pinball_debug.py
+++ src/pinball_debug.py
@@ -28,7 +28,7 @@ print type(ball)
 
 ball_NP = ball.copyTo(render)
 # ball_NP.setPos(0, 0, 0.45)
-ball_NP.setPos(3,0, 0.5)
+ball_NP.setPos(0,0, 0.01)
 #Setup the sphere's physics
 mass = OdeMass()
 mass.setSphere(50, 0.1)
@@ -44,62 +44,45 @@ ball_geom.setBody(ball_body)
 sphere = wireGeom().generate ('sphere', radius=0.1)
 sphere.setPos (ball_NP.getPos(render))
 sphere.reparentTo(render)
-# sphere.setHpr (0, 0, 0)
 
 #extract the cube from the egg file
 egg2 = loader.loadModel("models/box_in_edit_mode_plane2.egg")
 plane1 = egg2.find("**/Plane.001")
-# print "plane1", plane1, type(plane1)
 plane1.reparentTo(render)
 plane2 = egg2.find("**/Plane.002")
-print "plane2", plane2, type(plane2)
 plane2.reparentTo(render)
 plane3 = egg2.find("**/Plane.003")
-print "plane3", plane3, type(plane3)
 plane3.reparentTo(render)
 plane4 = egg2.find("**/Plane.004")
-print "plane4", plane4, type(plane4)
 plane4.reparentTo(render)
 plane5 = egg2.find("**/Plane.005")
-print "plane5", plane5, type(plane5)
 plane5.reparentTo(render)
 
+
+
 def add_plane_to_physics(planeNP, space, params1, params2, params3, params4):
-    #create the plane
-    #set the position
-    #set the collide/category bits
     plane = OdePlaneGeom(space, params1, params2, params3, params4)
     pos = planeNP.getPos(render)
     plane.setCollideBits(BitMask32(0x00000002))
     plane.setCategoryBits(BitMask32(0x00000001))
-    # print plane.convertToPlane()
-    # print plane, type(plane)
-    # print planeNP.getPos(render), type(planeNP.getPos(render))
-    # print planeNP.getQuat(render), type(planeNP.getQuat(render))
-    # plane.setPosition(0, 0, 0)
-    # plane.setQuaternion(planeNP.getQuat(render))
     plane.setCollideBits(BitMask32(0x00000001))
     plane.setCategoryBits(BitMask32(0x00000002))
     return plane
 
+def add_wall_to_physics(space, dimx, dimy, dimz, locx, locy, locz):
+    box = OdeBoxGeom(space, dimx, dimy, dimz)
+    box.setPosition(locx, locy, locz)
+    box.setCollideBits(BitMask32(0x00000002))
+    box.setCategoryBits(BitMask32(0x00000001))
+    box.setCollideBits(BitMask32(0x00000001))
+    box.setCategoryBits(BitMask32(0x00000002))
+    return box
+
 ground_plane = add_plane_to_physics(plane1, space1, 0, 0, 1, 0)
-wall2 = add_plane_to_physics(plane2, space1, 12, -20, 0, 1)
-wall3 = add_plane_to_physics(plane2, space1, -12, 20, 0, 45)
-# wall2 = add_plane_to_physics(plane2, space1, 12, -20, 0, 0)
-# wall2 = add_plane_to_physics(plane2, space1, 1, 1, 1, 1)
-# wall2 = add_plane_to_physics(plane2, space1, )
-#this is the equation of the x-y plane... so with physics enabled, the ball should sit on this surface
-# plane = OdePlaneGeom(space1, 0, 0, 1, 0)
-# plane.setCollideBits(BitMask32(0x00000002))
-# plane.setCategoryBits(BitMask32(0x00000001))
-# modelTrimesh = OdeTriMeshData(plane1, True)
-# modelGeom = OdeTriMeshGeom(space1, modelTrimesh)
-# modelGeom.setCollideBits(BitMask32(0x00000002))
-# modelGeom.setCategoryBits(BitMask32(0x00000001))
-# cube = egg.find("**/Cube")
-# print cube.getName()
-# print type(cube)
-# cube.reparentTo(render)  # make this stuff visible
+wall_west = add_wall_to_physics(space1, 10, 0.1, 2, 0, -3, 1)
+wall_east = add_wall_to_physics(space1, 10, 0.1, 2, 0, 3, 1)
+wall_north = add_wall_to_physics(space1, 0.1, 5, 2, -5, 0, 1)
+wall_south = add_wall_to_physics(space1, 1, 5, 2, 5, 0, 1)
 
 def set_light():
     # Create Ambient Light
@@ -145,7 +128,7 @@ def simulationTask(task):
     sphere.setPos(ball_body.getPosition())
     # ball_body.set_force(0,176.689,-1000)
     # ball_body.set_force(0,0, -100)
-    # ball_body.setForce(0, 1, -1)
+    ball_body.setForce(1, 0, 0)
     # ball_body.setForce(1, 1, 0)
     contactgroup1.empty()  # Clear the contact joints
     return task.cont
